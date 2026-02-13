66951701f8165e62d7f664d17bfb367625a2b3a8
Ganesh Kokare <gkokare193@gmail.com>
2026-02-13 14:27:52 +0530

    Add files via upload

---
+++ NewLine.java
@@ -0,0 +1,13 @@
+
+public class NewLine {
+	public static void main(String[] args) {
+		System.out.println("Ganesh");
+		System.out.println("Kokare");
+
+		System.out.print("Ganesh");
+		System.out.print("Kokare");
+		
+		System.out.println("Ganesh");
+		System.out.print("Kokare");
+	}
+}
