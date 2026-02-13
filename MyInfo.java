70c6d810f091715a13cc43926c42bb149e3a70de
Ganesh Kokare <gkokare193@gmail.com>
2026-02-13 14:27:23 +0530

    Add files via upload

---
+++ MyInfo.java
@@ -0,0 +1,11 @@
+
+public class MyInfo {
+	public static void main(String[] args) {
+		System.out.println("First Name: Ganesh");
+		System.out.println("Middle Name: Ashok");
+		System.out.println("Surname: Kokare");
+		System.out.println("Age: 21");
+		System.out.println("Occupation: Student");
+		System.out.println("Education: Pursuing Bsc(C.S)");
+	}
+}
