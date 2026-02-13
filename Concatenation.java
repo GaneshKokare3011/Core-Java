db93f788c9ed022063fb10b5b2ccd51eb47069a0
Ganesh Kokare <gkokare193@gmail.com>
2026-02-13 14:28:16 +0530

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,11 @@
+
+public class Concatenation {
+	public static void main(String[] args) {
+		System.out.println("Ganesh" + 2 + 2);
+		System.out.println("Ganesh" + (2 + 2));
+		System.out.println(2 + 2 + "Ganesh");
+		System.out.println("Ganesh" + -4 + 8);
+		System.out.println(2 + 2 + "Ganesh" + (-4 + 8));
+		System.out.println(2 + 2 + "Ganesh" + 4 * 3);
+	}
+}
