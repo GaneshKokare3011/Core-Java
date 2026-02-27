f21f8729d80374e67414db9c07aac76839547590
Ganesh Kokare <gkokare193@gmail.com>
2026-02-27 14:02:43 +0530

    Add files via upload

---
+++ Operators/Relational Operators/RelationalOperators.java
@@ -0,0 +1,22 @@
+package com.operator;
+
+public class RelationalOperators {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 10;
+		
+		System.out.println(a > b); //false
+		System.out.println(a < b); //true
+		System.out.println(a >= b); //false
+		System.out.println(a <= b); //true
+		System.out.println(a > c); //false
+		System.out.println(a < c); //false
+		System.out.println(a >= c); //true
+		System.out.println(a <= c); //true
+		System.out.println(a == c); //true
+		System.out.println(a != c); //false
+		System.out.println(a == b); //false
+		System.out.println(a != b); //true
+	}
+}
