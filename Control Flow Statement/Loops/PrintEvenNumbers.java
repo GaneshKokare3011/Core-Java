921a3e7af237565049d8ebe8edb017d2909ccbf0
Ganesh Kokare <gkokare193@gmail.com>
2026-03-13 12:06:16 +0530

    Add files via upload

---
+++ Control Flow Statement/Loops/PrintEvenNumbers.java
@@ -0,0 +1,27 @@
+package com.loops;
+
+public class PrintEvenNumbers {
+	public static void main(String[] args) {
+
+		// for loop
+//		for (int i = 1; i <= 50; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+		// while loop
+//		int i = 1;
+//		while (i <= 50) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+		// do while
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 50);
+	}
+}
