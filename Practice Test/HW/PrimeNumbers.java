7bd921db9531caa9455bc08de26635e882d70fed
Ganesh Kokare <gkokare193@gmail.com>
2026-03-23 15:35:02 +0530

    Update PrimeNumbers.java

--- Practice Test/HW/PrimeNumbers.java
+++ Practice Test/HW/PrimeNumbers.java
@@ -2,18 +2,18 @@ package com.practice;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int limit = 10; // Set the upper limit
+        int limit = 10; 
 
         System.out.println("Prime numbers up to " + limit + " are:");
 
         for (int number = 2; number <= limit; number++) {
-            boolean isPrime = true; // Assume number is prime
+            boolean isPrime = true; 
 
             // Check divisibility from 2 to √number
             for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) {
-                    isPrime = false; // Found a divisor, not prime
-                    break; // No need to check further
+                    isPrime = false; 
+                    break; 
                 }
             }
 
