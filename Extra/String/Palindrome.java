49e6a354d2543c550c64102d60cedf051a7636b9
Ganesh Kokare <gkokare193@gmail.com>
2026-05-22 20:34:20 +0530

    Add Palindrome checker in Palindrome.java

---
+++ Extra/String/Palindrome.java
@@ -0,0 +1,18 @@
+class Palindrome {
+    public static void main(String[] args) {
+        String input = "madam";
+        String reversed = "";
+        
+        for (int i = input.length() - 1; i >= 0; i --) {
+            char ch = input.charAt(i);
+            
+            reversed += ch;
+        }
+        
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+}
