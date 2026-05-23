182ab792bdd981a0c44317b7d67006f94a9178a4
Ganesh Kokare <gkokare193@gmail.com>
2026-05-23 21:23:32 +0530

    Add SizeOfList.java to demonstrate ArrayList usage
    Implement a Java program to demonstrate ArrayList size and elements.

---
+++ Extra/Collections/SizeOfList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class SizeOfList {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		
+		System.out.println("List size: " + list.size());
+		System.out.println("First element: " + list.getFirst());
+		System.out.println("Last element: " + list.getLast());
+	}
+}
