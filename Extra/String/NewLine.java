b003d80bfbca2f14df28c2435ed8dcbe9820b7f4
Ganesh Kokare <gkokare193@gmail.com>
2026-05-24 23:02:03 +0530

    Add NewLine class to print characters of a string

---
+++ Extra/String/NewLine.java
@@ -0,0 +1,11 @@
+class NewLine {
+	public static void main(String[] args) {
+		String input = "Core";
+		
+		for (int i = 0; i < input.length(); i ++) {
+			char ch = input.charAt(i);
+		
+			System.out.println(ch);
+		}
+	}
+}
