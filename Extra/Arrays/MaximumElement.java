95704f3f668e85963a0590f65d8060c470f21e9c
Ganesh Kokare <gkokare193@gmail.com>
2026-05-24 23:07:32 +0530

    Add MaximumElement class to find max in array

---
+++ Extra/Arrays/MaximumElement.java
@@ -0,0 +1,13 @@
+class MaximumElement {
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 3, 40, 4, 5};
+		int max = arr[0];
+		
+		for (int i = 0; i < arr.length; i ++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println("Maximum element from given array: " + max);
+	}
+}
