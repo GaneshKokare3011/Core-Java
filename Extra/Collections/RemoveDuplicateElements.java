5fadfcc5a21b031ca1391f24dab45aface1ae8b5
Ganesh Kokare <gkokare193@gmail.com>
2026-05-24 23:37:17 +0530

    Add RemoveDuplicateElements.java to filter duplicates
    Implemented a Java program to remove duplicate elements from an ArrayList.

---
+++ Extra/Collections/RemoveDuplicateElements.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class RemoveDuplicateElements {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(3);
+		list.add(5);
+		
+		for (int i = 0; i < list.size(); i ++) {
+			for (int j = i + 1; j < list.size(); j ++) {
+				if (list.get(i).equals(list.get(j))) {
+					list.remove(j);
+					j --;
+				}
+			}
+		}
+		System.out.println("List after removing duplicates: " + list);
+	}
+}
