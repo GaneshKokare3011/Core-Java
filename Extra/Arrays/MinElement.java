6aef27a9c613effa371031056b5ad975e141986e
Ganesh Kokare <gkokare193@gmail.com>
2026-05-24 23:10:15 +0530

    Add MinElement class to find minimum in array

---
+++ Extra/Arrays/MinElement.java
@@ -0,0 +1,13 @@
+class MinElement {
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 3, 4, 5};
+		int min = arr[0];
+
+		for (int i = 0; i < arr.length; i ++) {
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		System.out.println("Minimum element from given array: " + min);
+	}
+}
