4e9b0a2e3fd17ecf274a9ef70f4d27d21c888cc9
Ganesh Kokare <gkokare193@gmail.com>
2026-05-26 20:20:20 +0530

    Add files via upload

---
+++ Practice Test/Test_2/CharFrequencyUsingHMap.java
@@ -0,0 +1,25 @@
+package com.practice1;
+
+import java.util.HashMap;
+
+public class CharFrequencyUsingHMap {
+	public static void main(String[] args) {
+		String str = "banana";
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		for (int i = 0; i < str.length(); i ++) {
+			char ch = str.charAt(i);
+			
+			if (map.containsKey(ch)) {
+				map.put(ch, map.get(ch) + 1);
+			} else {
+				map.put(ch, 1);
+			}
+		}
+		
+		for (char key : map.keySet()) {
+			System.out.println(key + " -> " + map.get(key));
+		}
+	}
+}
