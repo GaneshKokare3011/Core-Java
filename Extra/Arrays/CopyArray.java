40637f32dedb43db7947efba547842d59b9aea80
Ganesh Kokare <gkokare193@gmail.com>
2026-05-27 22:56:28 +0530

    Add CopyArray class to demonstrate array copying

---
+++ Extra/Arrays/CopyArray.java
@@ -0,0 +1,15 @@
+class CopyArray {
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 3, 4, 5};
+		int arr2[] = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i ++) {
+			arr2[i] = arr[i];
+		}
+
+		System.out.println("Array 2: ");
+		for (int i = 0; i < arr2.length; i ++) {
+			System.out.print(arr2[i] + " ");
+		}
+	}
+}
