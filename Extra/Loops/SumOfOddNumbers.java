d84b7560da407a5c5ab79a8a470f59ccd8bd4a93
Ganesh Kokare <gkokare193@gmail.com>
2026-05-27 22:51:04 +0530

    Add SumOfOddNumbers class to calculate sum of odds

---
+++ Extra/Loops/SumOfOddNumbers.java
@@ -0,0 +1,11 @@
+class SumOfOddNumbers { 
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 100; i ++) {
+			if (i % 2 != 0) {
+				sum += i;
+			}
+		}
+		System.out.println("Sum of odd numbers between 1-100 is: " + sum);
+	}
+}
