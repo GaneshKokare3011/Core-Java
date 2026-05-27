15a3a7ee1e3bbdd4cabdbd2cbeed1a79a13caa67
Ganesh Kokare <gkokare193@gmail.com>
2026-05-27 22:36:25 +0530

    Add ContainsWord class to check for substring

---
+++ Extra/String/ContainsWord.java
@@ -0,0 +1,11 @@
+class ContainsWord {
+	public static void main(String[] args) {
+		String input = "java is a programming language";
+		
+		if (input.contains("java")) {
+			System.out.println("The String contains the word java");
+		} else {
+			System.out.println("The String does not contain the word java");
+		}
+	}
+}
