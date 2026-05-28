71c7e4dcb353d4f254632a55efa7b7c7b76cf8fc
Ganesh Kokare <gkokare193@gmail.com>
2026-05-28 23:41:34 +0530

    Add SBInsertAtMiddle.java to insert text in StringBuilder

---
+++ Extra/String/SBInsertAtMiddle.java
@@ -0,0 +1,7 @@
+class SBInsertAtMiddle {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("I love programming");
+		sb.insert(7, "Java ");
+		System.out.println(sb);
+	}
+}
