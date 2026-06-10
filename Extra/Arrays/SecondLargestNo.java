87df714bd80d67f7e6737f55494ea7d6e2c66407
Ganesh Kokare <gkokare193@gmail.com>
2026-06-10 22:50:18 +0530

    Add SecondLargestNo class to find second largest number

---
+++ Extra/Arrays/SecondLargestNo.java
@@ -0,0 +1,20 @@
+class SecondLargestNo {
+	public static void main(String[] args) {
+		
+		int arr[] = {5, 4, 3, 2, 1};
+		
+		for (int i = 0; i < arr.length - 1; i ++) {
+			for (int j = i + 1; j < arr.length; j ++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		int secondLargest = arr[arr.length - 2];
+
+		System.out.println("The second largest element is: " + secondLargest);
+	}
+}
